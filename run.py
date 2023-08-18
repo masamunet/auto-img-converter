fc20fc528124cca793890037866323940554e299
Masamune Utsunomiya <masamunet@gmail.com>
2023-08-18 12:21:18 +0900

    test

--- run.py
+++ run.py
@@ -116,6 +116,7 @@ def any2img(uri, endpoint, params):
     payload = params.copy()
     url = uri + endpoint
     image_base64, res_info = get_any2img(url, payload)
+    print(res_info)
     infotexts = res_info["infotexts"]
     r_seed = res_info["seed"]
     return infotexts, image_base64, r_seed
